a21057e7dec66dd2c9a1ee1a2de07401e6bf887b
Muhammad Sarim Mehdi <36595832+sarimmehdi@users.noreply.github.com>
2025-09-04 00:14:13 +0200

    fallback to java 22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,12 +29,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_23
-        targetCompatibility = JavaVersion.VERSION_23
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
     }
     kotlin {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_23)
+            jvmTarget.set(JvmTarget.JVM_22)
         }
     }
 }
